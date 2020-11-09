d8840be45714f6903b7e26815b4f4aad12fd60a7
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-09 22:29:43 +0100

    Update portfolio definition
    Differentiate between a portfolio and a portfolio item

--- portfolio.go
+++ portfolio.go
@@ -2,10 +2,14 @@ package main
 
 type Url = string
 
-type Portfolio struct {
+type PortfolioItem struct {
 	HeaderImage Url
 	Title       string
 	Description string
 	Media       []Url
 	Content     string
+}
+
+type Portfolio struct {
+	Items []PortfolioItem
 }
